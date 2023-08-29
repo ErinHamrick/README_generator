10bbac1c23917b382157ff64c9fa655af753ea28
Erin Hamrick <erin.hamrick76@gmail.com>
2023-08-29 16:27:10 -0500

    inde js file completed

--- index.js
+++ index.js
@@ -1,13 +1,80 @@
-// TODO: Include packages needed for this application
+const fs = require("fs");
+const generateMarkdown = require("./generateMarkdown");
+const inquirer = require("inquirer");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+function init() {
+	inquirer
+		.prompt([
+			{
+				type: "input",
+				name: "title",
+				message: "Enter the title of your project:",
+			},
+			{
+				type: "input",
+				name: "description",
+				message: "Provide a brief description of your project:",
+			},
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+			{
+				type: "input",
+				name: "instal",
+				message: "Explain installation process:",
+			},
+			{
+				type: "input",
+				name: "usage",
+				message: "Provide instructions and examples for use:",
+			},
+			{
+				type: "input",
+				name: "contribute",
+				message: "Guidelines for project contribution:",
+			},
+			{
+				type: "input",
+				name: "test",
+				message: "Provide examples on how to run tests:",
+			},
 
-// TODO: Create a function to initialize app
-function init() {}
+			{
+				type: "list",
+				name: "license",
+				message: "Add a license:",
+				choices: [
+					"MIT License",
+					"Apache License 2.0",
+					"GNU General Public License (GPL) 3.0",
+					"none",
+				],
+			},
 
-// Function call to initialize app
+			{
+				type: "input",
+				name: "github",
+				message: "Enter a link to your GitHub profile:",
+			},
+
+			{
+				type: "input",
+				name: "email",
+				message: "Enter your email address:",
+			},
+		])
+		.then((answers) => {
+			const readmeContent = generateMarkdown(answers);
+
+			fs.writeFile("README.MD", readmeContent, (err) => {
+				if (err) {
+					console.error("error writing README.md:", err);
+				} else {
+					console.log("README.md has been successfully generated!");
+				}
+			});
+		})
+		.catch((error) => {
+			console.error("Error:", error);
+		});
+}
+
 init();
